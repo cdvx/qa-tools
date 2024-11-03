15912069b62e9ae5a3e6175d02702cb88736875c
cdvx <cedriclusiba@gmail.com>
2024-11-03 21:05:32 +0300

    isolated teature testing with tunneling

---
+++ expose-with-ngrok.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+DOCKER_COMPOSE_FILE="docker-compose.yaml"
+LOCAL_PORT=8080
+
+
+echo "Starting the Docker Compose application..."
+docker-compose -f $DOCKER_COMPOSE_FILE up -d
+
+# wait for app to start
+sleep 30
+
+
+echo "Starting ngrok to expose http://127.0.0.1:${LOCAL_PORT}..."
+ngrok http ${LOCAL_PORT} > /dev/null &
+
+
+NGROK_PID=$!
+
+# wait for ngrok to initialization
+sleep 5
+
+
+NGROK_URL=$(curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+
+# ngrok URL
+echo "application is available at: $NGROK_URL"
+
+# when user terminats app, clean up
+read -p "Press ENTER to stop ngrok and shut down Docker Compose..."
+
+
+kill $NGROK_PID
+
+# clean upp
+docker-compose -f $DOCKER_COMPOSE_FILE down
+
+echo "application stopped."
